76a5f23cca0b6f987ab213e30b5d85def8b2286e
YosephAwoke <yosephawoke8@gmail.com>
2026-03-09 04:15:38 +0800

    Added home page

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,3 @@
+export default function Auth() {
+  return <div className="home">Auth Page</div>;
+}
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,3 @@
+export default function Checkout() {
+  return <div className="home">Checkout Page</div>;
+}
