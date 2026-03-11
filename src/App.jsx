03c664f6e90aff421c6a5b2d4c1824afa705f5dd
YosephAwoke <yosephawoke8@gmail.com>
2026-03-12 01:38:33 +0800

    add authentication context and auth page with form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
-import Checkout from './pages/checkout'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/checkout";
+import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
-
   return (
-      <div className='app'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<Auth />} />
-          <Route path='/checkout' element={<Checkout />} />
-        </Routes>
-       
-      </div>
-     
-    
-  )
+    <AuthProvider>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,114 @@
+import { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/AuthContext";
+
 export default function Auth() {
-  return <div className="home">Auth Page</div>;
+  const [mode, setMode] = useState("signup");
+  const [error, setError] = useState(null);
+
+  const { signUp, user, logout, login } = useContext(AuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    setError(null);
+    let result;
+    if (mode === "signup") {
+      result = signUp(data.email, data.password);
+    } else {
+      result = login(data.email, data.password);
+    }
+
+    // console.log(result);
+    if (result.success) {
+      alert("Success!");
+    } else {
+      setError(result.error);
+    }
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          {user && <p>User logged in: {user.email}</p>}
+          <button className="btn btn-secondary" onClick={() => logout()}>
+            Logout
+          </button>
+          <h1 className="page-title">
+            {mode === "signup" ? "Sign Up" : "Login"}
+          </h1>
+          <form className="auto-form" onSubmit={handleSubmit(onSubmit)}>
+
+            {error && <div className="error-message">{error}</div>}
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-input"
+                type="email"
+                id="email"
+                name="email"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (<span className="form-error">{errors.email.message}</span>)}
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: "Password must be less than 12 characters",
+                  },
+                })}
+                className="form-input"
+                type="password"
+                id="password"
+                name="password"
+                required
+              />
+              {errors.password && (<span className="form-error">{errors.password.message}</span>)}
+
+
+            </div>
+            
+            <button className="btn btn-primary btn-large " type="submit">
+              {mode === "signup" ? "Sign Up" : "Login"}
+            </button>
+          </form>
+
+          <div className="auth-switch">
+            {mode === "signup" ? (
+              <p>
+                Already have an account?{" "}
+                <span className="auth-link" onClick={() => setMode("login")}>
+                  Login
+                </span>
+              </p>
+            ) : (
+              <p>
+                Don't have an account?{" "}
+                <span className="auth-link" onClick={() => setMode("signup")}>
+                  Sign Up
+                </span>
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
