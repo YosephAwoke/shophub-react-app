b33d101cf8e6247de06a943b7d731b5ca65a77ad
YosephAwoke <yosephawoke8@gmail.com>
2026-03-19 23:49:09 +0800

    product details

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
+import { useContext } from "react";
 
-export const AuthContext = createContext(null);
+const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(
@@ -52,4 +53,10 @@ export default function AuthProvider({ children }) {
       {children}
     </AuthContext.Provider>
   );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
 }
